789cf0b6e4b5dae3896286ed270cb37f3f7c11d9
Jorge Acosta <jorge.acosta@externos.bci.cl>
2021-02-13 09:32:49 -0300

    Create Project

---
+++ SwiftUIRedux/SwiftUIReduxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIReduxApp.swift
+//  SwiftUIRedux
+//
+//  Created by Jorge Acosta on 13-02-21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIReduxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
