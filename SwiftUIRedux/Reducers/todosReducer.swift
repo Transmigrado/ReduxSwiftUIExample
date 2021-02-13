bb50c7edc89ab1df02b7b03abd6a5f11f237d4a1
Jorge Acosta <jorge.acosta@externos.bci.cl>
2021-02-13 12:01:26 -0300

    Fix Style

--- SwiftUIRedux/Reducers/todosReducer.swift
+++ SwiftUIRedux/Reducers/todosReducer.swift
@@ -5,14 +5,12 @@
 //  Created by Jorge Acosta on 13-02-21.
 //
 
-
 import ReSwift
-
 
 func todosReducer(action: Action, state: AppState?) -> AppState {
    
     var state = state ?? AppState()
-    
+
     switch action {
         case _ as AddTodo:
             state.todos.list.append(Todo(name: "Nuevo"))
